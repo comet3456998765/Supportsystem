33a24b2e923548d4acd0b0dcf1ae702fa275f908
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-02-12 11:52:03 -0600

    Update file.lua

--- file.lua
+++ file.lua
@@ -7,17 +7,14 @@ Notify({
     Title = " Loading IDs System ";
     Duration = 5;
 });
-wait("4")
 Notify({
     Description = " Zeus, Electron, Comet 3, Fluxus, Oxygen U ";
     Title = " Loading IDs System ";
     Duration = 4;
 });
-wait(3)
 Notify({
-    Description = " Krnl (No support adm for mobiles 2) ";
-    Title = " Loading IDs System (5 Sec) ";
+    Description = " Krnl (No support adm for mobiles 2 & 3) ";
+    Title = " Loading IDs System ";
     Duration = 3;
 });
-wait("5")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/SKHub-Orion/main/YesIDsSystem.lua"))()
